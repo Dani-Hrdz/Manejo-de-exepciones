ab17dcf02a68c5c4dafbdac07b9a96269edcf13b
Dani-Hrdz <105619891+Dani-Hrdz@users.noreply.github.com>
2022-05-24 11:46:38 -0700

    Exepciones programa2
    El cuadrado de un numero

---
+++ PROGRAMA-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROGRAMA_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            try
+            {
+                Console.Write("Ingresa un numero entero: ");
+                num = int.Parse(Console.ReadLine());
+                Console.WriteLine("El valor del cuadrado es: " + Math.Pow(num,2));
+            }
+            catch(FormatException x)
+            {
+                
+                Console.WriteLine(x.Message);
+            }
+            catch(OverflowException x)
+            {
+                Console.WriteLine(x.Message);
+            }
+            catch(NullReferenceException x)
+            {
+                Console.WriteLine(x.Message);
+            }
+            Console.WriteLine("Presiona enter para salir del programa");
+            Console.ReadKey();
+        }
+    }
+}
